3c8dfd477e9dd4b37c8390216ab301fa6e74e55b
Amirhosein Gharaati <amirgh1380@gmail.com>
2023-04-06 19:57:34 +0330

    Lint Board.svelte

--- src/routes/Board.svelte
+++ src/routes/Board.svelte
@@ -5,176 +5,166 @@
         - Refactor Cell to contain its own row and column
 -->
 <script lang="ts">
-    import Cell from './Cell'
+	import Cell from './Cell';
 
-    let SIZE = 8;
-    const bombDensity = 0.15;
+	let SIZE = 8;
+	const bombDensity = 0.15;
 
-    let board: Cell[][];
+	let board: Cell[][];
 
-    // make a new SIZExSIZE array
-    board = Array.from(
-        { length: SIZE },
-        () => Array.from(
-            { length: SIZE },
-            () => new Cell()    
-        )
-    );
+	// make a new SIZExSIZE array
+	board = Array.from({ length: SIZE }, () => Array.from({ length: SIZE }, () => new Cell()));
 
-    const randomBool = () => (Math.random() < 1 - bombDensity) ? false : true
-    board.forEach(row => row.forEach(cell => cell.isBomb = randomBool()));
+	const randomBool = () => (Math.random() < 1 - bombDensity ? false : true);
+	board.forEach((row) => row.forEach((cell) => (cell.isBomb = randomBool())));
 
-    $: {
-        if (board.every(row => row.every(cell => !cell.isBomb || cell.flagged))) {
-            alert("you've won!");
-        }
-    }
+	$: {
+		if (board.every((row) => row.every((cell) => !cell.isBomb || cell.flagged))) {
+			alert("you've won!");
+		}
+	}
 
-    function onClickCell(i: number, j:number) {
-        if (board[i][j].isBomb) {
-            alert("game over!");
-        }
-        board[i][j].clicked = true;
-        if (countNeighborBombs(i, j) === 0) {
-            propogateEmpty(i, j);
-        }
-    }
-    
-    function onRightClickCell(i: number, j:number) {
-        board[i][j].flagged = !board[i][j].flagged;
-    }
+	function onClickCell(i: number, j: number) {
+		if (board[i][j].isBomb) {
+			alert('game over!');
+		}
+		board[i][j].clicked = true;
+		if (countNeighborBombs(i, j) === 0) {
+			propogateEmpty(i, j);
+		}
+	}
 
-    function cellContent(i: number, j: number) {
-        const n = countNeighborBombs(i, j);
+	function onRightClickCell(i: number, j: number) {
+		board[i][j].flagged = !board[i][j].flagged;
+	}
 
-        return (n === 0) ? "" : n.toString()
-    }
+	function cellContent(i: number, j: number) {
+		const n = countNeighborBombs(i, j);
 
-    function countNeighborBombs(i: number, j: number): number {
-        return neighbors(i, j)
-                  .filter(cell => cell.isBomb)
-                  .length;
-    }
+		return n === 0 ? '' : n.toString();
+	}
 
-    function neighbors(i: number, j: number): Cell[] {
-        const arr = [];
-        for (let dx of [-1, 0, 1]) {
-            for (let dy of [-1, 0, 1]) {
-                let ii = i + dx;
-                let jj = j + dy;
+	function countNeighborBombs(i: number, j: number): number {
+		return neighbors(i, j).filter((cell) => cell.isBomb).length;
+	}
 
-                // ignore self
-                if (ii === 0 && jj === 0) {
-                    continue;
-                }
+	function neighbors(i: number, j: number): Cell[] {
+		const arr = [];
+		for (let dx of [-1, 0, 1]) {
+			for (let dy of [-1, 0, 1]) {
+				let ii = i + dx;
+				let jj = j + dy;
 
-                if (insideBounds(ii, jj)) {
-                    arr.push(board[ii][jj]);
-                }
-            }
-        }
+				// ignore self
+				if (ii === 0 && jj === 0) {
+					continue;
+				}
 
-        return arr;
-    }
+				if (insideBounds(ii, jj)) {
+					arr.push(board[ii][jj]);
+				}
+			}
+		}
 
-    function insideBounds(i: number, j: number): boolean {
-        return 0 <= i 
-              && i < SIZE
-              && 0 <= j
-              && j < SIZE;
-    }
+		return arr;
+	}
 
-    // reveal emptiness across connected component
-    function propogateEmpty(i: number, j: number) {
-        board[i][j].flagged = false;
-        board[i][j].clicked = true;
+	function insideBounds(i: number, j: number): boolean {
+		return 0 <= i && i < SIZE && 0 <= j && j < SIZE;
+	}
 
-        for (let dx of [-1, 0, 1]) {
-            for (let dy of [-1, 0, 1]) {
-                let ii = i + dx;
-                let jj = j + dy;
+	// reveal emptiness across connected component
+	function propogateEmpty(i: number, j: number) {
+		board[i][j].flagged = false;
+		board[i][j].clicked = true;
 
-                let shouldPropogate = insideBounds(ii, jj);
-                shouldPropogate &&= !board[ii][jj].isBomb;
-                shouldPropogate &&= !board[ii][jj].clicked;
+		for (let dx of [-1, 0, 1]) {
+			for (let dy of [-1, 0, 1]) {
+				let ii = i + dx;
+				let jj = j + dy;
 
-                if (shouldPropogate) {
-                    propogateEmpty(ii,jj);
-                }
-            }
-        }
-    }
+				let shouldPropogate = insideBounds(ii, jj);
+				shouldPropogate &&= !board[ii][jj].isBomb;
+				shouldPropogate &&= !board[ii][jj].clicked;
 
+				if (shouldPropogate) {
+					propogateEmpty(ii, jj);
+				}
+			}
+		}
+	}
 </script>
 
 <!-- <input type="range" bind:value={SIZE} min={1} max={9} /> -->
 <svelte:body on:contextmenu|preventDefault />
 
-<div class="board"
-    style:grid-template-columns={`repeat(${SIZE}, 1fr)`}
-    style:grid-template-rows={`repeat(${SIZE}, 1fr)`}>
-    {#each board as row, i}
-        {#each row as cell, j}
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <span class="item"
-                on:contextmenu={() => onRightClickCell(i, j)}
-                on:click={() => onClickCell(i, j)}
-                class:flagged={board[i][j].flagged}
-                class:clicked={board[i][j].clicked}>
-                { cell.clicked ? cellContent(i, j) : "" }
-            </span>
-        {/each}
-    {/each}
+<div
+	class="board"
+	style:grid-template-columns={`repeat(${SIZE}, 1fr)`}
+	style:grid-template-rows={`repeat(${SIZE}, 1fr)`}
+>
+	{#each board as row, i}
+		{#each row as cell, j}
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<span
+				class="item"
+				on:contextmenu={() => onRightClickCell(i, j)}
+				on:click={() => onClickCell(i, j)}
+				class:flagged={board[i][j].flagged}
+				class:clicked={board[i][j].clicked}
+			>
+				{cell.clicked ? cellContent(i, j) : ''}
+			</span>
+		{/each}
+	{/each}
 </div>
 
 <style>
+	.board {
+		background: #222;
+		padding: 2%;
+		border-radius: 5%;
+		width: 700px;
+		height: 700px;
 
-.board {
-    background: #222;
-    padding: 2%;
-    border-radius: 5%;
-    width: 700px;
-    height: 700px;
+		display: grid;
+		grid-gap: 0.5em;
+	}
 
-    display: grid;
-    grid-gap: 0.5em;
-}
+	.item {
+		width: 100%;
+		height: 100%;
+		padding: 2%;
+		border-radius: 25%;
+		background: #111;
+		transition: background-color 0.3s;
+	}
 
-.item {
-    width: 100%;
-    height: 100%;
-    padding: 2%;
-    border-radius: 25%;
-    background: #111;
-    transition: background-color .3s;
-}
+	.item:hover {
+		background-color: rgb(43, 2, 43);
+	}
 
-.item:hover {
-    background-color: rgb(43, 2, 43);
-}
+	.item.flagged {
+		background: url('$lib/images/flag2.png') #111;
+		image-rendering: crisp-edges;
+		background-size: 1.5em;
+		background-repeat: no-repeat;
+		background-position: center;
+	}
 
-.item.flagged {
-    background: url('$lib/images/flag2.png') #111;
-    image-rendering: crisp-edges;
-    background-size: 1.5em;
-    background-repeat: no-repeat;
-    background-position: center;
-}
+	.item.clicked {
+		background-color: #333;
+	}
 
-.item.clicked {
-    background-color: #333;
-}
+	span {
+		font-family: 'Minecraft';
+		font-weight: black;
+		font-size: 2em;
+		color: whitesmoke;
+		user-select: none;
 
-span {
-    font-family: 'Minecraft';
-    font-weight: black;
-    font-size: 2em;
-    color: whitesmoke;
-    user-select: none;
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
